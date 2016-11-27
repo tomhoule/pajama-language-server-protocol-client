17d22453688b82984aa734ae89127d380faaf01e
Tom Houlé <tom@tomhoule.com>
2016-11-27 09:41:51 +0100

    Update dependencies

--- src/codec.rs
+++ src/codec.rs
@@ -23,8 +23,8 @@ impl Codec for RpcCodec {
         }
     }
 
-    fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) {
-        json::to_writer(buf, &msg).unwrap();
+    fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> Result<(), io::Error> {
+        json::to_writer(buf, &msg).map_err(|err| io::Error::new(io::ErrorKind::Other, err))
     }
 }
 
